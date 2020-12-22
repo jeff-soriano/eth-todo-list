cfa23468f1a92e7e41392ded356fcc579d23ad95
Jeff Soriano <soriano.jeffm@gmail.com>
2020-12-22 12:39:27 -0800

    Render the tasks

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 App = {
+    loading: false,
+    contracts: {},
+
     load: async () => {
         await App.loadWeb3();
         await App.loadAccount();
+        await App.loadContract();
+        await App.render();
     },
 
     // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -38,8 +43,84 @@ App = {
     },
 
     loadAccount: async () => {
+        // Set the current blockchain account
         App.account = web3.eth.accounts[0];
-        console.log(App.account);
+    },
+
+    loadContract: async () => {
+        // Create a JavaScript version of the smart contract
+        const todoList = await $.getJSON("TodoList.json");
+        App.contracts.TodoList = TruffleContract(todoList);
+        App.contracts.TodoList.setProvider(App.web3Provider);
+
+        // Hydrate the smart contract with values from the blockchain
+        App.todoList = await App.contracts.TodoList.deployed();
+    },
+
+    render: async () => {
+        // Prevent double render
+        if (App.loading) {
+            return;
+        }
+
+        // Update app loading state
+        App.setLoading(true);
+
+        // Render account
+        $("#account").html(App.account);
+
+        // Render tasks
+        await App.renderTasks();
+
+        // Update app loading state
+        App.setLoading(false);
+    },
+
+    renderTasks: async () => {
+        // Load the total task count from the blockchain
+        const taskCount = await App.todoList.taskCount();
+        const $taskTemplate = $(".taskTemplate");
+
+        // Render out each task with a new task template
+        for (var i = 0; i < taskCount; i++) {
+            // Fetch task data from the blockchain
+            const task = await App.todoList.tasks(i);
+            const taskId = task[0].toNumber();
+            const taskContent = task[1];
+            const taskCompleted = task[2];
+
+            // Create html for the task
+            const $newTaskTemplate = $taskTemplate.clone();
+            $newTaskTemplate.find(".content").html(taskContent);
+            $newTaskTemplate.find("input")
+                .prop("name", taskId)
+                .prop("checked", taskCompleted);
+
+            // Put the task in the correct list
+            if (taskCompleted) {
+                $("#completedTaskList").append($newTaskTemplate);
+            } else {
+                $("#taskList").append($newTaskTemplate);
+            }
+
+            // Show the task
+            $newTaskTemplate.show();
+        }
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean;
+
+        const loader = $('#loader');
+        const content = $('#content');
+
+        if (boolean) {
+            loader.show();
+            content.hide();
+        } else {
+            loader.hide();
+            content.show();
+        }
     }
 }
 
